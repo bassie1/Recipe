3049e19a79e73a1ecba275c1cd20c4ad13d99a17
bassie1 <bassie423@gmail.com>
2026-09-01 16:05:25 -0400

    hw 22 recipe delete sproc

--- Database Files/Recipe Sprocs/RecipeDelete.sql
+++ Database Files/Recipe Sprocs/RecipeDelete.sql
@@ -1,8 +1,17 @@
 create or alter procedure dbo.RecipeDelete(
-@RecipeId int
+@RecipeId int,
+@Message varchar(500) = '' output
 )
 as 
 begin
+	declare @return int = 0
+
+	if not exists(select * from Recipe r where r.RecipeId = @RecipeId and (datediff(day, r.DateArchived, getdate()) >= 30 or r.RecipeStatus = 'drafted'))
+	begin
+		select @return = 1, @Message = 'Can only delete recipe that is drafted or archived for at least 30 days'
+		goto finished
+	end
+
 	begin try
 		begin tran
 		delete RecipeDirections where RecipeId = @RecipeId
@@ -14,4 +23,8 @@ begin
 		rollback;
 		throw
 	end catch
+
+	finished:
+	return @return
 end
+go
